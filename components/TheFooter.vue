23ff4170412c0e2693ad836c4dbbfa7cb1271a9f
Mohamed Hanini <mohamed.hanini95@gmail.com>
2020-06-27 13:36:12 +0100

    creation of components Footer & Header

---
+++ components/TheFooter.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+      <v-spacer></v-spacer>
+      <span> made with Love by Mohamed hanini </span>
+    </v-footer>
+</template>
+<script>
+export default {
+    name:"TheFooter"
+}
+</script>
---
+++ components/TheNavBar.vue
@@ -0,0 +1,59 @@
+<template>
+<div>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title v-text="title" />
+    </v-app-bar>
+    </div>
+</template>
+<script>
+export default {
+    name:"TheNavBar",
+    data () {
+    return {
+      clipped: true,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-home',
+          title: 'home',
+          to: '/'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'MogaMenuisier'
+    }
+  }
+}
+</script>
