0dee983d059cbf978634bb6fbca3aa430b332358
Mohamed Hanini <mohamed.hanini95@gmail.com>
2021-08-25 14:00:37 +0100

    update

--- components/TheWhatsappBtn.vue
+++ components/TheWhatsappBtn.vue
@@ -1,12 +1,30 @@
 <template>
-     <v-btn
-            fab
-            dark
-            fixed
-            bottom
-            left
-            color="#25D366"
-          >
-            <v-icon size="40" >mdi-whatsapp</v-icon>
-          </v-btn>
+  <v-btn
+    fab
+    dark
+    fixed
+    bottom
+    left
+    color="#25D366"
+    :href="getLinkWhastapp"
+  >
+    <v-icon size="40" >mdi-whatsapp</v-icon>
+  </v-btn>
 </template>
+<script>
+export default {
+  name:"",
+  data(){
+      return{
+        number:"0609889770", message:"Bonjour, j'ai eu votre numéro su moga menuisier"
+      }
+    },
+    computed:{
+      getLinkWhastapp() {
+        var url = 'https://wa.me/' 
+          + this.number 
+        return url
+        }
+    }
+}
+</script>
